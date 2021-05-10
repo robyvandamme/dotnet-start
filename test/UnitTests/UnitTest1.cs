f3b367c320560a5744c507e7a49e49da99c565ed
Roby Van Damme <roby@robyvandamme.com>
2021-05-10 13:56:47 +0700

    Add and configure example UnitTests project

---
+++ test/UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void THis_Should_Not_Show_Up_In_Warnings()
+        {
+        }
+    }
+}
